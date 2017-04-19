18a0f961deccb4b7634a33e4c2520c6bb5f2ec66
hinesboy <hines.zhu@gmail.com>
2017-04-19 12:58:51 +0800

    wrap , add btn

--- src/App.vue
+++ src/App.vue
@@ -11,13 +11,13 @@
       <h2 class="item-header">
         默认配置
       </h2>
-      <mavonEditor class="item-editor" v-model="help"></mavonEditor>
+      <mavonEditor @save="savedata" class="item-editor" v-model="help1"></mavonEditor>
     </div>
     <div class="item">
       <h2 class="item-header">
         自定义工具栏
       </h2>
-      <mavonEditor :toolbars="toolbars" class="item-editor" v-model="help"></mavonEditor>
+      <mavonEditor :toolbars="toolbars" class="item-editor" v-model="help2"></mavonEditor>
     </div>
     <div class="item">
       <span style="display: block;margin: 30px 0 15px 0;color: #1e6bb8" class="">
@@ -55,17 +55,22 @@ export default {
   name: 'app',
   data () {
     return {
-      help: HELP.help,
+      help1: HELP.help,
+      help2: HELP.help,
       toolbars: {
         underline: true, // 下划线
         strikethrough: true, // 中划线
-        table: true, // 表格
+        undo: true,
+        save: true,
         fullscreen: true, // 全屏编辑
         help: true // 帮助
       }
     }
   },
   methods: {
+    savedata () {
+      alert('save')
+    }
   },
   components: {
     'mavonEditor': mavonEditor
